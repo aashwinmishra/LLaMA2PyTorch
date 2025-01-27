d74f651c9f1bb3bee871a21fda8964a130ba3d9d
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2025-01-26 21:11:26 -0800

    Update model.py

--- model.py
+++ model.py
@@ -24,6 +24,26 @@ class ModelArgs:
   device: str = None
 
 
+def precompute_theta_pos_frequencies(head_dim: int, 
+                                     seq_len: int, 
+                                     device: str, 
+                                     theta: float=10000.0)->torch.tensor:
+  theta = torch.tensor([10000**(-2*(i-1)/head_dim) for i in range(head_dim//2)]).to(device)
+  m = torch.arange(seq_len, device=device)
+  freqs = torch.outer(m, theta).float()
+  return torch.polar(torch.ones_like(freqs), freqs)
+
+
+def apply_rotary_embeddings(x: torch.Tensor, 
+                            freqs_complex: torch.Tensor, 
+                            device: str)->torch.tensor:
+  x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+  freqs_complex = freqs_complex.unsqueeze(0).unsqueeze(2)
+  x_rotated = x_complex * freqs_complex
+  x_out = torch.view_as_real(x_rotated).reshape(*x.shape).type_as(x).to(device)
+  return x_out
+
+
 class Transformer(nn.Module):
   def __init__(self, args: ModelArgs) -> None:
     super().__init__()
